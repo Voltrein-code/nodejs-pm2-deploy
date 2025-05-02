4f6f220a3bee7419bb2fac1c94f64be6bf2b143b
Eugene Shalnev <mr-voltrein@yandex.ru>
2025-05-02 15:43:52 +0300

    WIP: Промежуточное сохраниение

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './env.deploy' });
+dotenv.config({ path: './.env.deploy' });
 
 const {
   DEPLOY_REPOSITORY, DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF,
